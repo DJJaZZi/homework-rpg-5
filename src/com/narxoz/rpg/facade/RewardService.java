2fce82e1b95106fc44cc2e00eda238e11a3e4e4a
DJJaZZi <siganak18@gmail.com>
2026-03-16 18:58:45 +0500

    RewardService modified

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -1,11 +1,16 @@
 package com.narxoz.rpg.facade;
 
+import com.narxoz.rpg.enemy.BossEnemy;
+import com.narxoz.rpg.hero.HeroProfile;
+
 public class RewardService {
-    public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
-        if (battleResult == null) {
-            return "TODO";
+    public void determineReward(HeroProfile hero, BossEnemy boss, AdventureResult result) {
+        if (hero.isAlive()) {
+            result.setWinner(hero.getName());
+            result.setReward("Epic Boss Chest & 1000 Gold");
+        } else {
+            result.setWinner(boss.getName());
+            result.setReward("None (Defeated)");
         }
-        return "TODO";
     }
 }
